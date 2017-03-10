64f5e62075c267254eea6e02f34119212123f267
Jackie Casper <Casper.Jaclyn@gmail.com>
2017-03-10 14:56:08 -0500

    key problems

--- controllers/api/controller.js
+++ controllers/api/controller.js
@@ -1,5 +1,8 @@
 const controller = {};
-const keys = require('../../config/keys');
+const keys = require('../../config/keys') || {
+  geoLocationKey: process.env.GEO_LOCATION_KEY,
+  placesKey: process.env.PLACES_KEY
+};
 const AuthService = require('../../services/auth');
 
 controller.getKeys = (req, res) => {
