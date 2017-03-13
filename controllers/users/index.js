4cf0a3fb2f5bbe61126e1e4440b51ad9c76044a7
Jackie Casper <Casper.Jaclyn@gmail.com>
2017-03-12 20:23:18 -0400

    fix logout

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -8,8 +8,9 @@ router.post('/login/', controller.test, controller.login);
 router.get('/signup/', AuthService.redirectUser, controller.showNew);
 router.post('/new/', controller.test, controller.new);
 router.get('/profile', AuthService.redirect, controller.showProfile);
-router.get('/:name', AuthService.redirect, controller.showUser);
 router.get('/logout', controller.logout);
+router.get('/:name', AuthService.redirect, controller.showUser);
+
 
 
 
