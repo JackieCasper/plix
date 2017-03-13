94d52c5a4c8886d38f950035f1d927107bc38b5c
Jackie Casper <Casper.Jaclyn@gmail.com>
2017-03-12 20:50:19 -0400

    trying to fix only one nearby result coming back

--- controllers/api/locations/controller.js
+++ controllers/api/locations/controller.js
@@ -22,46 +22,24 @@ controller.findNearby = (req, res) => {
   }
   Locations
     .findNearby(searchData.lat, searchData.lng, searchData.distance)
-    .then(nearbyLocations => {
+    .then(nearbyPlix => {
       const formattedPlix = [];
 
       console.log('      ------------------------------');
       console.log('      GOT NEARBY LOCATIONS');
-      console.log(nearbyLocations);
-      if (!nearbyLocations.length) {
+      console.log(nearbyPlix);
+      if (!nearbyPlix.length) {
         console.log('NO NEARBY LOCATIONS');
         res.json({
           plix: [],
           message: 'No Results'
         });
+      } else {
+        res.json({
+          plix: nearbyPlix;
+        })
       }
-      nearbyLocations.forEach(nearbyLocation => {
-        Locations
-          .findPlix(nearbyLocation.id)
-          .then(nearbyPlix => {
-            console.log('         ---------------------------');
-            console.log('         GOT PLIX');
-            console.log(nearbyPlix);
-            nearbyPlix.forEach(plix => {
-              plix.location = nearbyLocation;
-              formattedPlix.push(plix);
-            });
-            res.json({
-              plix: formattedPlix,
-              message: 'OK'
-            });
-          })
-          .catch(err => {
-            /////////////////////////
-            // SET CENTER LOCATION TO ERROR
-            /////////////////////////
-            console.log('         ---------------------------');
-            console.log('         ERROR GETTING PLIX');
-            console.log(err);
-          });
 
-
-      });
     })
     .catch(err => {
       console.log('------------------------');
--- models/locations.js
+++ models/locations.js
@@ -21,11 +21,7 @@ Location.findPlix = (id) => {
 //https://developers.google.com/maps/articles/phpsqlsearch_v3?csw=1
 Location.findNearby = (lat, lng, distance) => {
   return db.manyOrNone(`
-        SELECT ( 3959 * ACOS( COS( RADIANS($1) ) * COS( RADIANS( lat ) ) * COS( RADIANS( lng ) - RADIANS($2) ) + SIN( RADIANS($1) ) * SIN( RADIANS( lat ) ) ) ) AS distance, 
-        id, place_id, address, lat, lng FROM locations  
-        WHERE ( 3959 * ACOS( COS( RADIANS($1) ) * COS( RADIANS( lat ) ) * COS( RADIANS( lng ) - RADIANS($2) ) + SIN( RADIANS($1) ) * SIN( RADIANS( lat ) ) ) ) < $3
-        ORDER BY distance 
-        LIMIT 20`, [lat, lng, distance]);
+        SELECT ( 3959 * ACOS( COS( RADIANS($1) ) * COS( RADIANS( locations.lat ) ) * COS( RADIANS( locations.lng ) - RADIANS($2) ) + SIN( RADIANS($1) ) * SIN( RADIANS( locations.lat ) ) ) ) AS distance, locations.place_id, locations.address, locations.lat, locations.lng, plix.id, plix.thumb FROM plix JOIN locations ON locations.id = plix.location_id WHERE ( 3959 * ACOS( COS( RADIANS($1) ) * COS( RADIANS( locations.lat ) ) * COS( RADIANS( locations.lng ) - RADIANS($2) ) + SIN( RADIANS($1) ) * SIN( RADIANS( locations.lat ) ) ) ) < $3 ORDER BY distance LIMIT 20`, [lat, lng, distance]);
 }
 
 
